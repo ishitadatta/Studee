0c5e74b3c0517f780fc9ad3b68b6a84b8d2120b1
Ishita Datta <isheeta50@gmail.com>
2021-11-27 20:54:29 +0530

    Setting debug as true

--- Studee/settings.py
+++ Studee/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'i&pf2s!^^bpl#)7)r&=@&54a3%&*q6s%!-iqtyf_0%n=e8((lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['studee-app.herokuapp.com', '127.0.0.1']
 
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_URL = '/static/'
